963c6f6eb12088feaea5a0d86755d7180993ef09
Adrian <297513931@qq.com>
2020-03-11 18:35:16 +0800

    初始化项目.

---
+++ shaylee-api/src/main/java/com/shaylee/api/ShayleeApiApplication.java
@@ -0,0 +1,13 @@
+package com.shaylee.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShayleeApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShayleeApiApplication.class, args);
+    }
+
+}
